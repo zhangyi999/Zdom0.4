3d2e2ed50069e07162a2c3c9a7bc5cb6037b8e0f
zhangyi999 <zhang_btc@outlook.com>
2020-10-04 18:58:11 +0800

    测试 hooks 封装

--- src/page/Effect.js
+++ src/page/Effect.js
@@ -1,17 +1,28 @@
 import dom, {Components, useState} from 'dom';
 
-function Effect() {
+function deplay() {
+    return new Promise(r => setTimeout(r, 1000))
+}
+
+function uesEs(call) {
     const $ = useState(0)
-    $.useEffect(()=>{
-        console.log(this.props.a, 'useEffect')
-        if ( this.props.a === true ) {
+    $.useEffect( async ()=>{
+        console.log(call(), 'useEffect')
+        $.setState("loading...")
+        await deplay()
+        if ( call() === true ) {
             $.setState(1)
         } else {
             $.setState(0)
         }
-    },()=> this.props.a)
+    },call)
+    return $
+}
+
+function Effect() {
+    const $ = uesEs(() => this.props.a )
     return (
-        <span style="color:red">{$.state} {this.children}</span>
+        <span style="color:red"> {$.state} {this.children}</span>
     )
 }
 
